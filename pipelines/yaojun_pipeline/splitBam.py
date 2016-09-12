a5e84a9f0ce429c0e8b7bace7e8214844136fae6
wckdouglas <wckdouglas@gmail.com>
2016-09-11 21:42:36 -0500

    added cython script

--- pipelines/yaojun_pipeline/splitBam.py
+++ pipelines/yaojun_pipeline/splitBam.py
@@ -3,6 +3,9 @@
 import pysam
 import argparse
 import re
+import pyximport
+pyximport.install(setup_args={'include_dirs': pysam.get_include()})
+import parse_bam
 import numpy as np
 
 def getopt():
@@ -12,62 +15,6 @@ def getopt():
     parser.add_argument('-i', '--inBam', default='-', help = 'bam file name, or stdin (default: stdin)' )
     parser.add_argument('-o','--outprefix', required=True, help='output prefix ($OUTPUTPREFIX.unique.bam, $OUTPUTPREFIX.multi.bam)')
     return parser.parse_args()
-
-def softClipSize(aln):
-    # compare softclip size, output maximum softclipped base on either side
-    cigar = aln.cigarstring
-    cigarNum = np.array(re.findall('[0-9]+',cigar),dtype='int64')
-    cigarStr = np.array(re.findall('[A-Z]',cigar),dtype='string')
-    clipped = cigarNum[cigarStr == 'S']
-    return np.max(clipped) if len(clipped) > 0 else 0
-
-def bowtie2split(bam, uniquebam, multibam, id_file):
-    i = 0
-    while True:
-        try:
-            read1 = bam.next()
-            read2 = bam.next()
-            i += 2
-            assert read1.qname == read2.qname, 'Not paired end'
-            if not read1.is_unmapped and not read2.is_unmapped and \
-                    softClipSize(read1) < 20 and softClipSize(read2) < 20:
-                if read1.mapq == 255 and read2.mapq == 255:
-                    uniquebam.write(read1)
-                    uniquebam.write(read2)
-                else:
-                    multibam.write(read1)
-                    multibam.write(read2)
-            else:
-                id_file.write(read1.qname + '\n')
-            if i % 100000 == 0:
-                print 'Parsed %i alignments' %i
-        except StopIteration:
-            break
-    return 0
-
-def hisat2split(bam, uniquebam, multibam, id_file):
-    i = 0
-    while True:
-        try:
-            read1 = bam.next()
-            read2 = bam.next()
-            i += 2
-            assert read1.qname == read2.qname, 'Not paired end'
-            if not read1.is_unmapped and not read2.is_unmapped and \
-                    softClipSize(read1) < 20 and softClipSize(read2) < 20:
-                if read1.get_tag('NH') == 1 and read2.get_tag('NH') == 1:
-                    uniquebam.write(read1)
-                    uniquebam.write(read2)
-                else:
-                    multibam.write(read1)
-                    multibam.write(read2)
-            else:
-                id_file.write(read1.qname + '\n')
-            if i % 100000 == 0:
-                print 'Parsed %i alignments' %i
-        except StopIteration:
-            break
-    return 0
 
 def main():
     args = getopt()
@@ -80,9 +27,9 @@ def main():
                 pysam.Samfile(multiBam,'wb',template=inbam) as multibam, \
                 open(unmapped_id,'w') as id_file:
             if args.program == 'bowtie2':
-                bowtie2split(inbam, uniquebam, multibam, id_file)
+                parse_bam.bowtie2split(inbam, uniquebam, multibam, id_file)
             elif args.program == 'hisat2':
-                hisat2split(inbam, uniquebam, multibam, id_file)
+                parse_bam.hisat2split(inbam, uniquebam, multibam, id_file)
     return 0
 
 if __name__ == '__main__':
