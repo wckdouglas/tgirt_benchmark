5403e56e8397b8789756048b0db539f10d08f899
wckdouglas <wckdouglas@gmail.com>
2016-09-12 09:34:27 -0500

    fixed kallisto countpath

--- pipelines/alignment_free/kallisto.sh
+++ pipelines/alignment_free/kallisto.sh
@@ -3,7 +3,7 @@
 DATAPATH=${SCRATCH}/bench_marking/data
 INDEX_PATH=${REF}/human_transcriptome
 INDEX=${INDEX_PATH}/transcriptome
-RESULTPATH=${WORK}/cdw2854/bench_marking/alignment_free
+RESULTPATH=${SCRATCH}/cdw2854/bench_marking/alignment_free
 COUNT_PATH=${RESULTPATH}/countFiles
 BAM_PATH=${RESULTPATH}/bamFiles
 THREADS=24
@@ -14,7 +14,7 @@ do
 	R2=${R1/R1/R2}
 	SAMPLENAME=$(basename ${R1%_R1_001.fastq.gz})
 	echo kallisto quant \
-		-i ${INDEX} -o ${COUNTPATH}/${SAMPLENAME} \
+		-i ${INDEX} -o ${COUNT_PATH}/${SAMPLENAME} \
 		--fr-stranded \
 		${R1} ${R2} --pseudobam  \
 		\> ${BAM_PATH}/${SAMPLENAME}.bam
