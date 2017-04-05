015e808ecfa331368b5dc2187f57c0f8f5a3003e
wckdouglas <wckdouglas@gmail.com>
2017-04-04 21:25:36 -0500

    added fasta multimap reduce

--- download_data/check_ref.R
+++ download_data/check_ref.R
@@ -7,10 +7,38 @@ library(dplyr)
 bed_path <- '/stor/work/Lambowitz/ref/RNASeqConsortium'
 gene_bed <- bed_path %>%
     str_c('/genes.bed') %>%
-    read_tsv(col_names=c('chrom','start','end','gene_name','score','strand','bio_type','gene_id')) %>%
+    read_tsv(col_names=c('chrom','start','end','gene_name','score','strand','bio_type','gene_id'),
+             col_type = 'ciiccccc') %>%
+    filter(!is.na(chrom)) %>%
     tbl_df
 
 df <- read_tsv('/stor/work/Lambowitz/ref/human_transcriptome/transcripts.tsv') %>%
-     right_join(gene_bed)
+     right_join(gene_bed) %>%
+     filter(!is.na(t_id) | bio_type=='tRNA')
 
-df %>% filter(is.na(name))  %>% select(-chrom,-start,-end, -score, -strand) %>% View
+
+filter_bed <- function(type_pattern, filename){
+    bed_file_name <- str_c(bed_path, '/', filename,'.bed')
+    if (!grepl('genes_no', filename)){
+        gene_bed %>% 
+            filter(grepl(type_pattern, bio_type)) %>%
+            write_tsv(bed_file_name, col_names = F)
+    }else{
+        gene_bed %>%
+            filter(!grepl(type_pattern, bio_type)) %>%
+            write_tsv(bed_file_name, col_names=F)
+    }
+    message('Written ', bed_file_name)
+}
+
+patterns <- c('miRNA|misc_RNA|snoRNA|snRNA',
+              'protein_coding',
+             'miRNA|misc_RNA|snoRNA|snRNA|tRNA',
+             '5S_rRNA|18S_rRNA|5.8S_rRNA|28S_rRNA',
+             '18S_rRNA|28S_rRNA|5.8S_rRNA|5S_rRNA|miRNA|misc_RNA|rRNA|snoRNA|snRNA|tRNA')
+filenames <- c('sncRNA_no_tRNA',
+               'protein',
+              'sncRNA_x_protein.bed',
+              'rDNA', 
+              'genes_no_sncRNA_rRNA_tRNA')
+map2(patterns, filenames,filter_bed)
--- download_data/make_genome_ref.sh
+++ download_data/make_genome_ref.sh
@@ -2,6 +2,9 @@
 
 REF_PATH=$REF/RNASeqConsortium
 TRANSCRIPTOME_PATH=$REF/human_transcriptome
+HG19_HG38=http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
+piRNA=http://regulatoryrna.org/database/piRNA/download/archive/v1.0/bed/piR_hg19_v1.0.bed.gz
+curl $HG19_HG38 | gunzip >$REF_PATH/hg19Tohg38.chain
 
 
 #get gene annotations
@@ -21,4 +24,5 @@ zcat $REF_PATH/reference.fa.gz \
 	> reference.fa 
 cd $REF_PATH
 hisat2-build reference.fa reference.fasta
+
 
