427bdd71f867507160bbf267f5b4a4ebcfe9cba4
wckdouglas <wckdouglas@gmail.com>
2017-06-20 09:17:50 -0500

    modified map

--- pipelines/alignment_free/salmon/salmon.sh
+++ pipelines/alignment_free/salmon/salmon.sh
@@ -11,18 +11,21 @@ mkdir -p ${COUNT_PATH} ${BAM_PATH}
 
 for R1 in ${DATAPATH}/*.1.fq.gz
 do
-	R2=${R1/.1./.2.}
-	SAMPLENAME=$(basename ${R1%.1.fq.gz})
-	echo time salmon quant \
-		--seqBias --gcBias \
-		--index $INDEX --libType ISF \
-		--writeMappings  \
-		--threads=${THREADS} \
-		--auxDir $RESULTPATH/${SAMPLENAME} \
-		--numBootstraps 100 \
-		--mates1 ${R1} --mates2 ${R2} \
-		--output $RESULTPATH/${SAMPLENAME} \
-		\| samtools view -b@ $THREADS \
-		\> ${RESULTPATH}/${SAMPLENAME}.bam
+	for KMER in '' '_11' '_15' '_21'
+	do
+		R2=${R1/.1./.2.}
+		SAMPLENAME=$(basename ${R1%.1.fq.gz})
+		echo time salmon quant \
+			--seqBias --gcBias \
+			--index ${INDEX}${KMER} --libType ISF \
+			--writeMappings  \
+			--threads=${THREADS} \
+			--auxDir ${RESULTPATH}${KMER}/${SAMPLENAME} \
+			--numBootstraps 100 \
+			--mates1 ${R1} --mates2 ${R2} \
+			--output ${RESULTPATH}${KMER}/${SAMPLENAME} \
+			\| samtools view -b@ $THREADS \
+			\> ${RESULTPATH}${KMER}/${SAMPLENAME}.bam
+	done
 done
 
--- pipelines/genome_mapping/yaojun_pipeline/prepairCommands.sh
+++ pipelines/genome_mapping/yaojun_pipeline/prepairCommands.sh
@@ -2,27 +2,26 @@
 
 PROJECT_PATH=${SCRATCH}/bench_marking
 DATAPATH=${PROJECT_PATH}/data
-RESULT_PATH=${PROJECT_PATH}/genome_mapping
-REF_PATH=${REF}
-HUMAN_INDEX=${REF}/RNASeqConsortium/reference.fasta # GRCh38 hisat2 index 
-BED_PATH=${REF}/GRCh38/Bed_for_counts_only #bed file GRCh38
-tRNA_INDEX=${REF}/GRCh38/tRNA/temp/tRNA #tRNA with mtTRNA and cytosolic tRNA
-rRNA_INDEX=${REF}/GRCh38/Plasma_ref/rDNA #tRNA with mtTRNA and cytosolic tRNA
-SPLICE_FILE=${REF}/RNASeqConsortium/splicesite.txt
+RESULT_PATH=${PROJECT_PATH}/genome_mapping/tgirt_map
+HUMAN_INDEX=${REF}/benchmarking/GRCH38_genome/reference  # GRCh38 hisat2 index 
+BED_PATH=${REF}/benchmarking/human_transcriptome  #bed file GRCh38
+tRNA_INDEX=${REF}/benchmarking/human_transcriptome/tRNA #tRNA with mtTRNA and cytosolic tRNA
+rRNA_INDEX=${REF}/benchmarking/human_transcriptome/rRNA #tRNA with mtTRNA and cytosolic tRNA
+trRNA_INDEX=${REF}/benchmarking/human_transcriptome/tRNA_rRNA #tRNA with mtTRNA and cytosolic tRNA
+SPLICE_FILE=${REF}/benchmarking/GRCH38_genome/splicesite.txt
 THREADS=24
-ADAPTOR=adaptors.fa
-STRAND=forward
 
-for FQ in ${DATAPATH}/*R1_001.fastq.gz
+for FQ1 in ${DATAPATH}/*R1_001.fastq.gz
 do
-    echo time python -u tgirtPipelinePaired.py --fastq=${FQ} \
-			--outdir=${RESULT_PATH} \
-			--humanIndex=${HUMAN_INDEX} \
-		    --splicesite=${SPLICE_FILE} \
-			--tRNAindex=${tRNA_INDEX} \
-	    	--threads=${THREADS} \
-            --adaptors=${ADAPTOR} \
-			--strand=${STRAND} \
-			--bedpath=${BED_PATH} \
-			--rRNAindex=${rRNA_INDEX}
+	FQ2=${FQ1/R1/R2}
+    echo tgirt_count.py -1 ${FQ1} -2 ${FQ2}\
+			-o ${RESULT_PATH} \
+			-x ${HUMAN_INDEX} \
+			-y ${HUMAN_INDEX} \
+			-b ${BED_PATH} \
+		    -s ${SPLICE_FILE} \
+			-t ${tRNA_INDEX} \
+			-r ${rRNA_INDEX} \
+	    	-p ${THREADS} \
+			-e ${trRNA_INDEX}
 done
