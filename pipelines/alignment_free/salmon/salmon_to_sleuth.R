bd2402f18c349e376295eff9e1c1db3adae9caf6
wckdouglas <wckdouglas@gmail.com>
2018-01-11 14:18:36 -0600

    cleaned

--- pipelines/alignment_free/salmon/salmon_to_sleuth.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-
-library(wasabi)
-
-salmon_path <- '/stor/work/Lambowitz/cdw2854/bench_marking/alignment_free/salmon'
-samples <- list.files(path = salmon_path, pattern = '[1-3]$')
-sfdirs <- file.path(salmon_path, samples)
-prepare_fish_for_sleuth(sfdirs)
