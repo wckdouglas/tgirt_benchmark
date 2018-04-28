9abbeefdda52b0a429045f16d31835639972bdbb
wckdouglas <wckdouglas@gmail.com>
2018-04-28 17:29:31 -0500

    added files

--- download_data/rRNA_transcript_to_gene.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python
-from __future__ import print_function
-import sys
-for line in open(sys.argv[1]):
-    if line.startswith('>'):
-        tid = line.rstrip('\n').lstrip('>')
-        gid = tid.split(':')[0]
-        print('%s\t%s\t%s\trRNA' %(gid, gid, tid))
